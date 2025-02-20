c180646a8ad42f2ec8e4ad56fbb6e28cddbb36ee
Tradon <tradon@praneat.com>
2025-02-20 20:34:23 +0700

    fix: 🐛 fix CreateModal using form instand of CustomForm

--- src/sections/components/modal/CreateModal.tsx
+++ src/sections/components/modal/CreateModal.tsx
@@ -1,12 +1,10 @@
 'use client'
-import { FC, ReactNode, useState } from 'react'
+import { FC, ReactNode } from 'react'
 
 import Modal from './Modal'
-import Dropdown from '../dropdown/Dropdown'
-import { getCommunityEnumValues } from '@/sections/shared/enums/community.enum'
-import { Input, Textarea } from '@headlessui/react'
-import Button from '../button/Button'
+
 import Image from 'next/image'
+import CreateForm from '../form/CreateForm'
 
 interface Props {
     title: ReactNode
@@ -15,13 +13,11 @@ interface Props {
 }
 
 const CreateModal: FC<Props> = ({ title, isActive, setIsActive }: Props) => {
-    const [community, setCommunity] = useState<string>('Choose a community')
-
     return (
         <Modal isActive={isActive} setIsActive={setIsActive}>
             <div
                 className={
-                    `flex flex-col gap-[30px] bg-white px-4 py-[30px] rounded-xl  ` +
+                    `relative flex flex-col gap-[30px] bg-white px-4 py-[30px] rounded-xl  ` +
                     `md:max-w-[685px] md:p-[30px] md:mx-auto`
                 }
             >
@@ -36,35 +32,14 @@ const CreateModal: FC<Props> = ({ title, isActive, setIsActive }: Props) => {
                         height={10}
                     />
                 </div>
-
                 <div className="text-[28px] leading-6 font-semibold">
                     {title}
                 </div>
 
-                <div className="flex flex-col gap-3.5">
-                    <Dropdown
-                        menuList={getCommunityEnumValues()}
-                        dropdownValue={community}
-                        setDropdownValue={setCommunity}
-                        type="light"
-                    />
-                    <Input
-                        className="border-grey-100 border px-3.5 py-2.5 rounded-lg"
-                        type="text"
-                        placeholder="Title"
-                    />
-                    <Textarea
-                        className="border-grey-100 h-[234px] text-top border px-3.5 py-2.5 rounded-lg"
-                        placeholder="What's on your mind..."
-                    />
-                </div>
-
-                <div className="flex justify-end gap-3">
-                    <Button theme="light" onClick={() => setIsActive(false)}>
-                        Cancel
-                    </Button>
-                    <Button>Post</Button>
-                </div>
+                <CreateForm
+                    onSubmit={() => setIsActive(false)}
+                    onCancel={() => setIsActive(false)}
+                />
             </div>
         </Modal>
     )
