b0bdfcbd361db74aece3723a446c5761117ff0f8
Tradon <tradon@praneat.com>
2025-02-20 21:30:51 +0700

    fix: 🐛 remove not used

--- src/sections/shared/utils/getAuthCookie.helper.ts
+++
@@ -1,7 +0,0 @@
-import getConfig from 'next/config'
-
-export const getAuthCookiesName = (): string => {
-    const { publicRuntimeConfig } = getConfig()
-    const env: string = publicRuntimeConfig.environment
-    return env === 'prod' ? 'tokens' : `tokens_${env.toLowerCase()}`
-}
