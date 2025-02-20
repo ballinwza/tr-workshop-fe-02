3b67f75c320b03b5ede83319fc7dc87711038402
Tradon <tradon@praneat.com>
2025-02-20 21:31:25 +0700

    feat: 🎸 creat login logput

--- src/modules/user/adapter/inbound/store/user.store.ts
+++ src/modules/user/adapter/inbound/store/user.store.ts
@@ -1,15 +1,17 @@
+// 'user server'
 import { IUser } from '@/modules/user/domain/model/user.model'
 import { create } from 'zustand'
 import { UserRepository } from '../../outbound/repository/user.repository'
 import { UserUsecase } from '@/modules/user/application/usecase/user.usecase'
-// import Router from 'next/router'
-// import Cookies from 'js-cookie'
+import { axiosWithRules } from '@/sections/shared/utils/fetchRule.utils'
 
 interface userState {
     user: IUser | null
     fetchUser: () => Promise<void>
     inputUser: string
     setInputUser: (val: string) => void
+    login: () => Promise<void>
+    logout: () => Promise<void>
 }
 
 export const useUserStore = create<userState>((set, get) => ({
@@ -21,22 +23,38 @@ export const useUserStore = create<userState>((set, get) => ({
         set(() => ({
             user,
         }))
-
-        // Cookies.set(getAuthCookiesName(), JSON.stringify(tokens), {
-        //     expires: 7,
-        //     secure: true,
-        //     sameSite: 'strict',
-        //     domain: window?.location?.hostname ?? null,
-        // })
-        if (user !== null) {
-            console.log(user)
-            // window.location.href = '/blogs'
-        }
     },
     inputUser: '',
     setInputUser: (val: string) => {
         set(() => ({
             inputUser: val,
         }))
+    },
+    login: async () => {
+        try {
+            const res = await axiosWithRules.post('/auth/login', {
+                username: get().inputUser,
+            })
+
+            if (res.data.success) {
+                window.location.href = '/blogs'
+            }
+            return
+        } catch (error) {
+            console.error('Error on useUserStore.login :', error)
+            return
+        }
+    },
+    logout: async () => {
+        try {
+            const res = await axiosWithRules.post('/auth/logout', {})
+            if (res.data.success) {
+                window.location.href = '/blogs'
+            }
+            return
+        } catch (error) {
+            console.error('Error on useUserStore.logout :', error)
+            return
+        }
     },
 }))
--- src/modules/user/application/port/user.repository.port.ts
+++ src/modules/user/application/port/user.repository.port.ts
@@ -1,5 +1,5 @@
 import { IUser } from '../../domain/model/user.model'
 
 export interface IUserRepository {
-    getByUsername: (username: string) => Promise<IUser>
+    getByUsername: () => Promise<IUser>
 }
