828b4d3009dcbf635e3c2ce9e9fabc7e44dc0e80
Tradon <tradon@praneat.com>
2025-02-20 20:37:32 +0700

    feat: 🎸 create Placard save repo and handle error

--- src/modules/placard/adapter/outbound/repository/placard.repository.ts
+++ src/modules/placard/adapter/outbound/repository/placard.repository.ts
@@ -2,6 +2,7 @@ import { IPlacardRepository } from '@/modules/placard/application/port/placard.r
 import { IPlacard } from '@/modules/placard/domain/model/placard.model'
 import axios from 'axios'
 import { PlacardEntityMapper } from '../mapper/placard.mapper'
+import { axiosWithRules } from '@/sections/shared/utils/fetchRule.utils'
 
 export class PlacardRepository implements IPlacardRepository {
     async getAll(): Promise<IPlacard[]> {
@@ -12,21 +13,29 @@ export class PlacardRepository implements IPlacardRepository {
 
             return PlacardEntityMapper.toDomainList(response.data)
         } catch (error) {
-            console.error('Error PlacardRepository.getAll')
-            throw error
+            console.error('Error PlacardRepository.getAll : ', error)
+            return []
         }
     }
 
-    async getByPlacardId(id: string): Promise<IPlacard> {
+    async getByPlacardId(id: string): Promise<IPlacard | null> {
         try {
-            const response = await axios.get(
-                `http://localhost:8000/placard/find/${id}`,
-            )
+            const response = await axiosWithRules.get(`/placard/find/${id}`)
 
             return PlacardEntityMapper.toDomain(response.data)
         } catch (error) {
-            console.error('Error PlacardRepository.getByPlacardId')
-            throw error
+            console.error('Error PlacardRepository.getByPlacardId : ', error)
+            return null
+        }
+    }
+    async save(formValue: IPlacard): Promise<boolean> {
+        try {
+            await axiosWithRules.post(`/placard/save`, formValue)
+
+            return true
+        } catch (error) {
+            console.error('Error PlacardRepository.save : ', error)
+            return false
         }
     }
 }
--- src/modules/placard/application/port/placard.repository.port.ts
+++ src/modules/placard/application/port/placard.repository.port.ts
@@ -2,5 +2,6 @@ import { IPlacard } from '../../domain/model/placard.model'
 
 export interface IPlacardRepository {
     getAll: () => Promise<IPlacard[]>
-    getByPlacardId: (placardId: string) => Promise<IPlacard>
+    getByPlacardId: (placardId: string) => Promise<IPlacard | null>
+    save: (formValue: IPlacard) => Promise<boolean>
 }
