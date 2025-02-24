d240c01b7f38ce938734dd9e7168a431e94941e2
Tradon <tradon@praneat.com>
2025-02-24 22:19:58 +0700

    fix: 🐛 refactor all user

--- src/modules/user/adapter/inbound/store/user.store.ts
+++ src/modules/user/adapter/inbound/store/user.store.ts
@@ -1,23 +1,32 @@
 import { IUser } from '@/modules/user/domain/model/user.model'
 import { create } from 'zustand'
 import { UserRepository } from '../../outbound/repository/user.repository'
-import { GetUserUsecase } from '@/modules/user/application/usecase/getUser.usecase'
+
 import { axiosWithRules } from '@/sections/shared/utils/fetchRule.utils'
+import { FetchUserUsecase } from '@/modules/user/application/usecase/fetchUser.usecase'
+import { message } from 'antd'
 
 interface userState {
-    user: IUser | null
+    user: IUser
     fetchUser: () => Promise<void>
     inputUser: string
     setInputUser: (val: string) => void
     login: () => Promise<void>
     logout: () => Promise<void>
 }
+export const defaultUser: IUser = {
+    id: '',
+    profileImageUrl: '',
+    fullName: '',
+    username: '',
+}
 
 export const useUserStore = create<userState>((set, get) => ({
-    user: null,
+    user: defaultUser,
     fetchUser: async () => {
         const repo = new UserRepository()
-        const usecase = new GetUserUsecase(repo)
+        const usecase = new FetchUserUsecase(repo)
+
         const user = await usecase.handle()
 
         set(() => ({
@@ -34,6 +43,7 @@ export const useUserStore = create<userState>((set, get) => ({
         try {
             const res = await axiosWithRules.post('/auth/login', {
                 username: get().inputUser,
+                password: 'nothing',
             })
 
             if (res.data.success) {
@@ -41,6 +51,7 @@ export const useUserStore = create<userState>((set, get) => ({
             }
             return
         } catch (error) {
+            message.error('Invalid username')
             console.error('Error on useUserStore.login :', error)
             return
         }
@@ -48,6 +59,7 @@ export const useUserStore = create<userState>((set, get) => ({
     logout: async () => {
         try {
             const res = await axiosWithRules.post('/auth/logout', {})
+            localStorage.removeItem('user_info')
             if (res.data.success) {
                 window.location.href = '/blogs'
             }
--- src/modules/user/adapter/outbound/repository/user.repository.ts
+++ src/modules/user/adapter/outbound/repository/user.repository.ts
@@ -1,12 +1,28 @@
-import { IUserRepository } from '@/modules/user/application/port/user.repository.port'
+'user server'
 import { IUser } from '@/modules/user/domain/model/user.model'
 import { UserEntityMapper } from '../mapper/user.mapper'
-import { axiosWithRules } from '@/sections/shared/utils/fetchRule.utils'
+import Cookies from 'js-cookie'
+import axios from 'axios'
+import { IUserRepository } from '@/modules/user/application/port/user.repository.port'
 
 export class UserRepository implements IUserRepository {
-    async getUser(): Promise<IUser> {
+    async fetchUser(): Promise<IUser> {
         try {
-            const response = await axiosWithRules.get('user/find')
+            const cookies = Cookies.get('access_token')
+
+            const response = await axios.get('/user/find', {
+                baseURL: `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN_DEV}`,
+                withCredentials: true,
+                timeout: 5000,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Credentials': true,
+                    'Access-Control-Allow-Origin': `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN_DEV}`,
+                    'x-api-key': process.env.NEXT_PUBLIC_X_API_KEY,
+                    Authorization: `Bearer ${cookies}`,
+                },
+            })
+
             return UserEntityMapper.toDomain(response.data)
         } catch (error) {
             console.error('Error UserRepository')
