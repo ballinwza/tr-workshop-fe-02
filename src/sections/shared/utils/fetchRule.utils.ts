8ddff3cd302db2497e702a79dd1be4d90cc512a2
Tradon <tradon@praneat.com>
2025-02-26 01:29:46 +0700

    fix: 🐛 fix prod env

--- src/sections/shared/utils/fetchRule.utils.ts
+++ src/sections/shared/utils/fetchRule.utils.ts
@@ -1,25 +1,25 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
 export const axiosWithXApiKey = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN_DEV}`,
+    baseURL: `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN_PROD}`,
     withCredentials: true,
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Credentials': true,
-        'Access-Control-Allow-Origin': `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN_DEV}`,
+        'Access-Control-Allow-Origin': `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN_PROD}`,
         'x-api-key': process.env.NEXT_PUBLIC_X_API_KEY,
     },
 })
 
 export const axiosWithAuth = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN_DEV}`,
+    baseURL: `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN_PROD}`,
     withCredentials: true,
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Credentials': true,
-        'Access-Control-Allow-Origin': `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN_DEV}`,
+        'Access-Control-Allow-Origin': `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN_PROD}`,
         'x-api-key': process.env.NEXT_PUBLIC_X_API_KEY,
         Authorization: `Bearer ${Cookies.get('access_token')}`,
     },
